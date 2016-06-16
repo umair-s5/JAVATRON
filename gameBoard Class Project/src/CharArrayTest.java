16dd6c90df8b773f6ce7213fbf7660039dbacc99
hhscgups <chirayugupta@yahoo.com>
2016-06-16 16:51:53 +0000

    Add files via upload

--- gameBoard Class Project/src/CharArrayTest.java
+++ gameBoard Class Project/src/CharArrayTest.java
@@ -1,30 +1,66 @@
 
-import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import sun.audio.AudioData;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+import sun.audio.ContinuousAudioDataStream;
+
+import java.io.FileInputStream;
 import java.io.IOException;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
+import java.awt.Color;
 
 public class CharArrayTest extends JFrame
 {
 	 public static void main(String args[]) throws IOException
 	 {
 		JFrame f = new JFrame();
-		Gameboard game = new Gameboard(960, 500);
-		//game.setBackground(Color.BLACK);
-		Bike redBike = new Bike('r', 50, 50, 'u');	
-		game.setVerticalWall(redBike);
+		Gameboard grid = new Gameboard(960, 500);
 		
-        f.add(game);
+		//Bike redBike = new Bike('r', 50, 50, 'u');	
+		//grid.setVerticalWall(redBike);
+		
+        f.add(grid);	
         f.setBackground(Color.BLACK);
         f.setVisible(true);
         f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        startBGMusic();
+        
+      
 
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
 		//for(int i = 0; i < game.getVerticalWallArray().length; i++) 
 			//for(int j = 0; j < game.getVerticalWallArray()[0].length; j++)
 				
-	}
+	 }
+	 
+	 @SuppressWarnings("restriction")
+	 public static void startBGMusic() 
+	 { 
+	    //Plays the background music
+		//make a new AudioPlayer.
+		AudioPlayer myBackgroundPlayer = AudioPlayer.player;
+        ContinuousAudioDataStream myLoop = null;
+         
+  		//use a try block in case the file doesn't exist.
+        try 
+        {
+	        AudioStream myBackgroundMusic = new AudioStream(new FileInputStream("Derezzed.wav"));
+	        AudioData myData = myBackgroundMusic.getData();
+	        myLoop = new ContinuousAudioDataStream(myData);
+	    }
+        catch(Exception error)
+        {
+        	JOptionPane.showMessageDialog(null, "Invalid file!");
+        }
+        
+        myBackgroundPlayer.start(myLoop);
+     }
 }
