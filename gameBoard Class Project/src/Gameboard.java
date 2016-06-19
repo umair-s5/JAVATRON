eda1836479fddb224a2945ca62bb4385626018de
hhscgups <chirayugupta@yahoo.com>
2016-06-19 15:01:04 +0000

    Add files via upload
    the updated gameboard

--- gameBoard Class Project/src/Gameboard.java
+++ gameBoard Class Project/src/Gameboard.java
@@ -1,12 +1,30 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class Gameboard extends JPanel
+public class Gameboard extends JPanel implements KeyListener, ActionListener 
 {
 	char[][] horizontal;
 	char[][] vertical;
+	
+	int speed = 10; //called inside of the up, down, right, left methods.
+	
+	Timer t = new Timer(7, this);
+	
+	int x = 4, y = 32, velX = 10 , velY = 0; //starting x and y coordinates of the yellow bike path.
+	int x1 = 4, y1 = 252, velX1 = 10, velY1 = 0; //starting x and y coordinates of the cyan bike path.
+	
+	Rectangle yellow = new Rectangle(x,y,10,0);
+	Rectangle cyan = new Rectangle(x1,y1,10,0);
+	
+	
 	
 	public Gameboard(int r, int c)
 	{
@@ -19,6 +37,10 @@ public class Gameboard extends JPanel
 		for(int i = 0; i < vertical.length; i ++)
 			for(int j = 0; j < vertical[0].length; j++)
 				vertical[i][j] = 'e';
+		
+		t.start();
+		addKeyListener(this);
+	    setFocusable(true);
 	}
 	
 	public void setVerticalWall(Bike bike)
@@ -45,8 +67,7 @@ public class Gameboard extends JPanel
 	
 	public char getHorizontalWall(int r, int c)
 	{
-		return horizontal[r][c];
-		
+		return horizontal[r][c];	
 	}
 	
 	public char getVerticalWall(int r, int c)
@@ -57,7 +78,6 @@ public class Gameboard extends JPanel
 	public char[][] getHorizontalWallArray()
 	{
 		return horizontal;
-		
 	}
 	
 	public char[][] getVerticalWallArray()
@@ -65,19 +85,16 @@ public class Gameboard extends JPanel
 		return vertical;
 	}
 	
+	
+	//Paints the Tron Grid and the single yellow test Tron bike path
 	public void paint(Graphics g)
 	{
 		for(int i = 0; i < vertical.length; i++)
 		{
-			for(int j = 0; j < vertical[0].length; j+= 4)
+			for(int j = 0; j < vertical[0].length; j+= 2)
 			{
-				switch(vertical[i][j])
-				{
-					case 'r':
-						g.setColor(Color.RED);
-				}
 				g.drawLine(j*10 + 10, i*10 + 10, j*10 + 10,  i*10 + 20);
-				g.setColor(new Color(77,77,255));
+				g.setColor(Color.BLUE);
 				//77,173,212 --- more retro shade of neon blue
 				//77,77,255 --- more modern shade of neon blue
 				
@@ -86,19 +103,150 @@ public class Gameboard extends JPanel
 		
 		for(int i = 0; i < horizontal.length; i++)
 		{
-			for(int j = 0; j < horizontal[0].length; j+= 4)
+			for(int j = 0; j < horizontal[0].length; j+= 2)
 			{
-				switch(horizontal[i][j])
-				{
-					case 'r':
-						g.setColor(Color.RED);
-				}
-				g.drawLine(i*10 + 10, j*10 + 10, i*10 + 20, j*10 + 10);
-				g.setColor(new Color(77,77,255));
-				//77,173,212 --- more retro shade of neon blue
-				//77,77,255 --- more modern shade of neon blue
 				
+				g.drawLine(i*10 + 10, j*10 + 10, i*10 + 20, j*10 + 10);
+				g.setColor(Color.BLUE);			
 			}	
-		}
+		}	
+		//92,174,174 cyan
+		//255,255,51 yellow 
+		//147,74,157 purple
+		//96,180,102 green
+		
+		//yellow bike path
+		g.setColor(new Color(96,180,102)); 
+		g.fillRect(x, y, 10, 15);
+		
+		//cyan bike path		
+		g.setColor(new Color(92,174,174));
+		g.fillRect(x1, y1, 10, 15);
 	}
+
+	
+    public void actionPerformed(ActionEvent e)
+    {
+        repaint();
+        x += velX;
+        y += velY; 
+        x1 += velX1;
+        y1 += velY1; 
+        
+    }
+    
+    //following methods update the x and y velocities when called in the keyPressed() method.
+    public void up()
+    {
+        velX = 0;
+        velY = -speed;
+    }
+    public void down()
+    {
+        velX = 0;
+        velY = speed;
+    }
+    public void left()
+    {
+        velX = -speed;
+        velY = 0;
+    }
+    public void right()
+    {
+        velX = speed;
+        velY = 0;
+    }
+    
+    
+    
+    public void up1()
+    {
+        velX1 = 0;
+        velY1 = -speed;
+    }
+    public void down1()
+    {
+        velX1= 0;
+        velY1 = speed;
+    }
+    public void left1()
+    {
+        velX1= -speed;
+        velY1 = 0;
+    }
+    public void right1()
+    {
+        velX1 = speed;
+        velY1 = 0;
+    }
+    
+    public static boolean collide(Rectangle a, Rectangle b)
+    {	
+    	return a.intersects(b);
+    }
+    
+    public void postCollide()
+    {
+    	if(collide(cyan,yellow) == true)
+    	{
+    		t.stop();
+    	}
+    	
+    }
+
+    //Enables the user to move the paths with arrow keys. 
+    //Also inhibts the paths from travelling on non-grid area. ---- will not be in final game product
+   
+    @Override  
+    public void keyPressed(KeyEvent e)
+	{
+	     int code = e.getKeyCode();
+	     
+	     //Arrow Keys 
+	     if (code == KeyEvent.VK_UP)
+	     {
+	     	up();
+	     }
+	     if (code == KeyEvent.VK_DOWN)
+	     {
+	        down();
+	     }
+	     if (code == KeyEvent.VK_RIGHT)
+	     {
+	         right();
+	     }
+	     if (code == KeyEvent.VK_LEFT)
+	     {
+	         left();
+	     }	
+	     
+	     //WASD 
+	     if (code == KeyEvent.VK_W)
+	     {
+	     	up1();
+	     }
+	     if (code == KeyEvent.VK_S)
+	     {
+	        down1();
+	     }
+	     if(code == KeyEvent.VK_D)
+	     {
+	        right1();
+	     }
+	     if (code == KeyEvent.VK_A)
+	     {
+	        left1();
+	     }	
+	     
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e)
+	{	
+	}
+	@Override
+	public void keyReleased(KeyEvent e)
+	{	
+	}
+
 }
